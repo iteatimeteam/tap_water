21a1926a237b738357883213889a6625d13d35d7
lihailong <lihailong@zhizhangyi.com>
2019-07-18 15:52:59 +0800

    添加tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'TapWaterTabbar.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TapWaterTabbar(),
     );
   }
 }
